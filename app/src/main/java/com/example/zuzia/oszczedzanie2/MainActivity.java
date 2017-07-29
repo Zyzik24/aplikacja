3214b12cd7aedc5725b79d9ebed2c44faf496723
Zyzik <Zuzannadudzinska@wp.pl>
2017-07-29 18:30:30 +0200

    fgg

---
+++ app/src/main/java/com/example/zuzia/oszczedzanie2/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.zuzia.oszczedzanie2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    boolean visibilityList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.lista);
+        visibilityList=false;
+        listView.setVisibility(View.INVISIBLE);
+
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, getClassList(position));
+                startActivity(intent);
+
+            }
+        };
+
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+    public Class getClassList(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return Cele.class;
+
+            case 1:
+                return MojeUstawienia.class;
+
+            case 2:
+                return Statystyki.class;
+        }
+
+        return  null;
+    }
+
+    public void settingsClick(View view) {
+        if(visibilityList)
+        {
+            visibilityList=false;
+            listView.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            visibilityList=true;
+            listView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return true;
+    }
+}
